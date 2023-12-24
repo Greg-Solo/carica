37911d3235cfa0b32932f48b977f6eaa7ec64077
Greg-Solo <gregorysolo7@gmail.com>
2023-12-24 16:29:54 +0700

    fixed error undetected face stops program, moved to main.py

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ from pathlib import Path
 
 ENCODINGS_PATH = Path("encodings/encodings.pkl")
 TRAIN_PATH = Path("data/train")
+TEST_PATH = Path("data/test")
 
-encode("hog", ENCODINGS_PATH, TRAIN_PATH)
+# encode("hog", ENCODINGS_PATH, TRAIN_PATH)
 
-# for filepath in Path("modules/face_rec/test").glob("*"):
-#     recognize(filepath, "hog", Path("modules/face_rec/output/encodings.pkl"))
+for filepath in TEST_PATH.glob("*"):
+    _name, _bbox = recognize(filepath, "hog", ENCODINGS_PATH)
+    print(_name, _bbox)
--- modules/face_rec/face_module.py
+++ modules/face_rec/face_module.py
@@ -4,31 +4,8 @@ import face_recognition
 import pickle
 from collections import Counter
 
-# DEFAULT_ENCODINGS_PATH = Path("output/encodings.pkl")
 
-# Path("training").mkdir(exist_ok=True)
-# Path("output").mkdir(exist_ok=True)
-# Path("test").mkdir(exist_ok=True)
-
-
-# WRAPPERS
 def encode(
-    model: str,
-    encodings_location: Path,
-    train_location: Path,
-):
-    encode_known_faces(model, encodings_location, train_location)
-
-
-def recognize(
-    image_location: str,
-    model: str,
-    encodings_location: Path,
-):
-    recognize_faces(image_location, model, encodings_location)
-
-
-def encode_known_faces(
     model: str,
     encodings_location: Path,
     train_location: Path,
@@ -52,28 +29,39 @@ def encode_known_faces(
         pickle.dump(name_encodings, f)
 
 
-def recognize_faces(
+def recognize(
     image_location: str,
     model: str,
     encodings_location: Path,
 ) -> None:
+    name = []
+    bbox = []
+
     with encodings_location.open(mode="rb") as f:
         loaded_encodings = pickle.load(f)
 
     input_image = face_recognition.load_image_file(image_location)
 
     input_face_locations = face_recognition.face_locations(input_image, model=model)
-    input_face_encodings = face_recognition.face_encodings(
-        input_image, input_face_locations
-    )
 
-    for bounding_box, unknown_encoding in zip(
-        input_face_locations, input_face_encodings
-    ):
-        name = _recognize_face(unknown_encoding, loaded_encodings)
-        if not name:
-            name = "Unknown"
-        print(name, bounding_box)
+    if input_face_locations:
+        input_face_encodings = face_recognition.face_encodings(
+            input_image, input_face_locations
+        )
+
+        for bounding_box, unknown_encoding in zip(
+            input_face_locations, input_face_encodings
+        ):
+            _name = _recognize_face(unknown_encoding, loaded_encodings)
+            if not _name:
+                _name = "Unknown"
+            name.append(_name)
+            bbox.append(bounding_box)
+    else:
+        name.append("Undetected")
+        bbox.append(())
+
+    return name, bbox
 
 
 def _recognize_face(
@@ -88,3 +76,5 @@ def _recognize_face(
     )
     if votes:
         return votes.most_common(1)[0][0]
+    else:
+        return None
